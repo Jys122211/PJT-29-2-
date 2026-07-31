a54030863f96bb51969a264018e890d1b4427f2d
jieun lee <happyjieun1025@gmail.com>
2026-07-31 14:54:32 +0900

    자산등록_백 구현_1.4

--- scoula/backend/src/main/java/org/scoula/config/RootConfig.java
+++ scoula/backend/src/main/java/org/scoula/config/RootConfig.java
@@ -23,10 +23,9 @@ import javax.sql.DataSource;
         "org.scoula.board.mapper",
         "org.scoula.member.mapper",
         "org.scoula.profitLoss.mapper",
-        "org.scoula.deposit.service",
         "org.scoula.deposit.mapper",
 })
-@ComponentScan(basePackages = {"org.scoula.board.service", "org.scoula.member.service","org.scoula.profitLoss.service"})
+@ComponentScan(basePackages = {"org.scoula.board.service", "org.scoula.member.service","org.scoula.profitLoss.service", "org.scoula.deposit.service",})
 
 @EnableTransactionManagement
 public class RootConfig {
